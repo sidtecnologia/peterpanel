e200531f6f163c955b15bc4a42d4d7e970ec4eee
sidtecnologia <sidsoporte@proton.me>
2025-12-05 12:19:39 -0500

    V1.2

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  SB_URL: null,
-  SB_ANON_KEY: null,
+  SB_URL: "https://ndqzyplsiqigsynweihk.supabase.co",
+  SB_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kcXp5cGxzaXFpZ3N5bndlaWhrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODQyOTQ4MiwiZXhwIjoyMDc0MDA1NDgyfQ.LYocdE6jGG5B-0n_2Ke0nUpkrAKC7iBBRV7RmgjATD8",
   STORAGE_BUCKET: "donde_peter",
   DEFAULT_IMG: "https://placehold.co/40x40/f1f5f9/94a3b8?text=IMG"
 };
